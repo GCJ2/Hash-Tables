8bbb06e0d1f6ee663efb9f8b14a3d917050cc934
Greg Johnston <gcjohnston91@gmail.com>
2020-05-04 16:53:18 -0400

    MVP done with code comments

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,103 +1,118 @@
 class HashTableEntry:
-    """
-    Hash Table entry, as a linked list node.
-    """
+	"""
+	Hash Table entry, as a linked list node.
+	"""
 
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
+	def __init__(self, key, value):
+		self.key = key
+		self.value = value
+		self.next = None
 
 
 class HashTable:
-    """
-    A hash table that with `capacity` buckets
-    that accepts string keys
+	"""
+	A hash table that with `capacity` buckets
+	that accepts string keys
+	Implement this.
+	"""
 
-    Implement this.
-    """
+	def __init__(self, capacity):
+		self.capacity = capacity
+		self.storage = [None] * capacity
 
-    def fnv1(self, key):
-        """
-        FNV-1 64-bit hash function
+	def fnv1(self, key):
+		"""
+		FNV-1 64-bit hash function
 
-        Implement this, and/or DJB2.
-        """
+		Implement this, and/or DJB2.
+		"""
 
-    def djb2(self, key):
-        """
-        DJB2 32-bit hash function
+	def djb2(self, key):
+		"""
+		DJB2 32-bit hash function
 
-        Implement this, and/or FNV-1.
-        """
+		Implement this, and/or FNV-1.
+		"""
+		hash_output = 5381                                                  # set default hash to this number
+		for char in key:                                                    # for each character in the key
+			hash_output = ((hash_output << 5) + hash_output) + ord(char)    # shifts all bits 5 places to the left and adds the original hash to it
+		                                                                    # then adds to the hash the Unicode for each individual character
+		return hash_output
 
-    def hash_index(self, key):
-        """
-        Take an arbitrary key and return a valid integer index
-        between within the storage capacity of the hash table.
-        """
-        #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+	def hash_index(self, key):
+		"""
+		Take an arbitrary key and return a valid integer index
+		between within the storage capacity of the hash table.
+		"""
+		# return self.fnv1(key) % self.capacity
+		return self.djb2(key) % self.capacity
 
-    def put(self, key, value):
-        """
-        Store the value with the given key.
+	def put(self, key, value):
+		"""
+		Store the value with the given key.
 
-        Hash collisions should be handled with Linked List Chaining.
+		Hash collisions should be handled with Linked List Chaining.
 
-        Implement this.
-        """
+		Implement this.
+		"""
+		index = self.hash_index(key)    # After creating the index through the hashing algorithm
+		self.storage[index] = value     # Put the value that was passed in at that index
 
-    def delete(self, key):
-        """
-        Remove the value stored with the given key.
+	def delete(self, key):
+		"""
+		Remove the value stored with the given key.
 
-        Print a warning if the key is not found.
+		Print a warning if the key is not found.
 
-        Implement this.
-        """
+		Implement this.
+		"""
+		index = self.hash_index(key)    # Find the index for the value from the key via hashing algorithm
+		self.storage[index] = None      # Set whatever is at that current index to None
 
-    def get(self, key):
-        """
-        Retrieve the value stored with the given key.
+	def get(self, key):
+		"""
+		Retrieve the value stored with the given key.
 
-        Returns None if the key is not found.
+		Returns None if the key is not found.
 
-        Implement this.
-        """
+		Implement this.
+		"""
+		index = self.hash_index(key)                # Find the index created through the hashing algorithm
+		return self.storage[index]                  # Return what is at that index
 
-    def resize(self):
-        """
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
+	def resize(self):
+		"""
+		Doubles the capacity of the hash table and
+		rehash all key/value pairs.
 
-        Implement this.
-        """
+		Implement this.
+		"""
+
 
 if __name__ == "__main__":
-    ht = HashTable(2)
+	ht = HashTable(2)
 
-    ht.put("line_1", "Tiny hash table")
-    ht.put("line_2", "Filled beyond capacity")
-    ht.put("line_3", "Linked list saves the day!")
+	ht.put("line_1", "Tiny hash table")
+	ht.put("line_2", "Filled beyond capacity")
+	ht.put("line_3", "Linked list saves the day!")
 
-    print("")
+	print("")
 
-    # Test storing beyond capacity
-    print(ht.get("line_1"))
-    print(ht.get("line_2"))
-    print(ht.get("line_3"))
+	# Test storing beyond capacity
+	print(ht.get("line_1"))
+	print(ht.get("line_2"))
+	print(ht.get("line_3"))
 
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+	# Test resizing
+	old_capacity = len(ht.storage)
+	ht.resize()
+	new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+	print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    print(ht.get("line_1"))
-    print(ht.get("line_2"))
-    print(ht.get("line_3"))
+	# Test if data intact after resizing
+	print(ht.get("line_1"))
+	print(ht.get("line_2"))
+	print(ht.get("line_3"))
 
-    print("")
+	print("")
