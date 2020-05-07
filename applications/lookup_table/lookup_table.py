35904fe113b46e17cfd33e48515a6f8b3e300dac
Greg Johnston <gcjohnston91@gmail.com>
2020-05-07 09:17:07 -0400

    Stuck on lowering run time on factorials

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,14 +1,17 @@
 import math
 import random
 
-look_up_table = {}
-for i in range(21):
-	look_up_table[i] = []
-for i in range(len(look_up_table)):
-	for j in range(1, 6):
-		look_up_table[i].append(i ** j)
-	print(f'Look up table {i}: {look_up_table[i]}')
 
+
+# look_up_table = {}
+# for i in range(21):
+# 	look_up_table[i] = []
+# for i in range(len(look_up_table)):
+# 	for j in range(1, 6):
+# 		look_up_table[i].append(i ** j)
+# 	print(f'Look up table {i}: {look_up_table[i]}')
+#
+# print(look_up_table[3][2])
 
 def slowfun(x, y):
 	"""
@@ -22,17 +25,36 @@ def slowfun(x, y):
 	"""
 
 	# TODO: Modify to produce the same results, but much faster
-	v = math.pow(x, y)
+	# v = math.pow(x, y)
 	# v = math.factorial(v)
 	# v //= (x + y)
 	# v %= 982451653
 	# return v
 
-	print(look_up_table)
+	powers_table = {}
+	for i in range(21):
+		powers_table[i] = []
+	for i in range(len(powers_table)):
+		for j in range(1, 7):
+			powers_table[i].append(i ** j)
+		# print(f'Look up table {i}: {look_up_table[i]}')
+
+	# factorial_table = {}
+	# for i in range(248832):
+	# 	print(i)
+	# 	factorial_table[i] = i ** i
+		# print((factorial_table[i] // (x + y)) % 982451653)
+	# print(look_up_table[x][y])
+	v = powers_table[x][y-1]
+	print(v)
+	v = v ** v
+	print(v)
+	return v
+
 
 # Do not modify below this line!
-
-# for i in range(50000):
-# 	x = random.randrange(2, 14)
-# 	y = random.randrange(3, 6)
-# 	print(f'{i}: {x},{y}: {slowfun(x, y)}')
+#
+for i in range(50000):
+	x = random.randrange(2, 14)
+	y = random.randrange(3, 6)
+	print(f'{i}: {x},{y}: {slowfun(x, y)}')
